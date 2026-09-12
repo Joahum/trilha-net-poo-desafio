1a1c84c9a2161b3b5c4797ed5d3efcc541bc597b
Joahum <rocha.schwarz@outlook.com>
2026-09-12 17:30:04 -0300

    test: add manual tests for Nokia and Iphone instances

--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia X30 5G", "123456789012345", 128);
+Iphone iphone = new Iphone("987654321", "Iphone 12", "987654321098765", 256);
+
+Console.WriteLine($"Nokia - Número: {nokia.Numero}, Modelo: {nokia.Modelo}, IMEI: {nokia.IMEI}, Memória: {nokia.Memoria}MB");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine($"Iphone - Número: {iphone.Numero}, Modelo: {iphone.Modelo}, IMEI: {iphone.IMEI}, Memória: {iphone.Memoria}MB");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+try
+{
+    var nokiaInvalido = new Nokia("", "Nokia 3310", "123456789012345", 128);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Erro esperado capturado: {ex.Message}");
+}
+
+try
+{
+    var iphoneInvalido = new Iphone("987654321", "Iphone 12", "", 256);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Erro esperado capturado: {ex.Message}");
+}
